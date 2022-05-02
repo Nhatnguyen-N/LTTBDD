88c1e03dc65da0627e693d74294ed94973908372
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2022-05-02 21:07:26 +0700

    finish lab2

---
+++ app/src/main/java/com/example/myapplication/MainActivity_2b.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity_2b extends AppCompatActivity {
+    Button btn_login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_b);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity_2c.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity_2c extends AppCompatActivity {
+    Button btn_login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_c);
+
+
+
+    }
+}
